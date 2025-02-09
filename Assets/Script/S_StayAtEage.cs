8b198b6e7db7bde7fbabc9a9f2ca5040562e9826
Grrrrretz <lhj15733527022@gmail.com>
2025-02-09 15:41:45 -0500

    start AS2 and make basic map

---
+++ Assets/Script/S_StayAtEage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_StayAtEdge : MonoBehaviour
+{
+    public Transform uimove;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 bottomL = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));//Get the world coordinates in the bottom left corner of the screen
+
+
+        uimove.position = new Vector3(bottomL.x + 2, 0, 0);
+    }
+}
